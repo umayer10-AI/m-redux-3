8761a11621013309c749e93827654f6a59162f24
umayer10-AI <mdumayer10@gmail.com>
2026-05-22 09:56:25 +0600

    add

--- src/redux/slice2.js
+++ src/redux/slice2.js
@@ -9,7 +9,6 @@ const slice = createSlice({
     initialState,
     reducers: {
         setName: (v,action) => {
-            console.log(v.ss)
             v.ss = action.payload
             console.log(v.ss)
         }
