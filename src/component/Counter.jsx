8dc159eb80ff763d44c235da61af069fd42e7a45
umayer10-AI <mdumayer10@gmail.com>
2026-05-22 08:13:07 +0600

    add

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Counter = () => {
     return (
         <div>
-            Umayer Ahmad
+            <h2 className='text-2xl font-bold'>Umayer Ahmad</h2>
+            <div className='m-5 space-y-2'>
+                <h2>Count: </h2>
+                <button className='py-1 px-3 rounded-2xl bg-blue-900 text-white'>Click</button>
+            </div>
         </div>
     );
 };
