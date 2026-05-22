5fbc7a3e32053befc6bbbe9baf6613aa6372443c
umayer10-AI <mdumayer10@gmail.com>
2026-05-22 09:50:20 +0600

    add

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Counter from '@/component/Counter';
 import Display from '@/component/Display';
+import Naming from '@/component/Naming';
 import React from 'react';
 
 const page = () => {
@@ -7,6 +8,7 @@ const page = () => {
     <div>
       <Counter></Counter>
       <Display></Display>
+      <Naming></Naming>
     </div>
   );
 };
