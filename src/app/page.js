638fad51dc66f210a092856c6be2bbf2c0503a28
umayer10-AI <mdumayer10@gmail.com>
2026-05-22 08:25:04 +0600

    add

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Counter from '@/component/Counter';
+import Display from '@/component/Display';
 import React from 'react';
 
 const page = () => {
   return (
     <div>
       <Counter></Counter>
+      <Display></Display>
     </div>
   );
 };
--- src/component/Display.jsx
+++ src/component/Display.jsx
@@ -1,9 +1,16 @@
+"use client"
 import React from 'react';
 
 const Display = () => {
     return (
         <div>
-            
+            <div className='m-5'>
+                <h2 className='text-xl font-bold'>My task: </h2>
+                <div className='flex items-center gap-2'>
+                    <input type="text" placeholder='Input Text' className='border py-1 rounded-xl' />
+                    <button className='py-1 px-3 rounded-2xl bg-green-600 text-white'>Add</button>
+                </div>
+            </div>
         </div>
     );
 };
