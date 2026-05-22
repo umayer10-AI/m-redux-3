515916c709b59ce07209e08e9d6166fda7eb6b54
umayer10-AI <mdumayer10@gmail.com>
2026-05-22 08:20:33 +0600

    add

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -1,12 +1,21 @@
+"use client"
+import { increment } from '@/redux/slice';
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Counter = () => {
+
+    const cnt = useSelector(v => v.counter.value)
+    console.log(cnt)
+
+    const dispatch = useDispatch()
+
     return (
         <div>
             <h2 className='text-2xl font-bold'>Umayer Ahmad</h2>
             <div className='m-5 space-y-2'>
-                <h2>Count: </h2>
-                <button className='py-1 px-3 rounded-2xl bg-blue-900 text-white'>Click</button>
+                <h2>Count: {cnt}</h2>
+                <button onClick={() => dispatch(increment())} className='py-1 px-3 rounded-2xl bg-blue-900 text-white'>Click</button>
             </div>
         </div>
     );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterProvider from "./slice"
 
 export const store = configureStore({
     reducer:{
-        
+        counter: counterProvider
     }
 })
