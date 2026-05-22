40552f3bff9a5b5b28fe345a1955e98b2b46bcb2
umayer10-AI <mdumayer10@gmail.com>
2026-05-22 09:48:31 +0600

    add

---
+++ src/redux/slice2.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState= {
+    ss: ''
+}
+
+const slice = createSlice({
+    name: "name",
+    initialState,
+    reducers: {
+        setName: (v,action) => {
+            console.log(v.ss)
+        }
+    }
+})
+
+export const { setName } = 
