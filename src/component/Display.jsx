c0d219ee4c77d9e66578c0214ff9038b581a2001
umayer10-AI <mdumayer10@gmail.com>
2026-05-22 08:26:57 +0600

    add

--- src/component/Display.jsx
+++ src/component/Display.jsx
@@ -1,14 +1,21 @@
 "use client"
-import React from 'react';
+import React, { useState } from 'react';
 
 const Display = () => {
+
+    const [s,setS] = useState('')
+
+    const a = () => {
+        console.log(s)
+    }
+
     return (
         <div>
             <div className='m-5'>
                 <h2 className='text-xl font-bold'>My task: </h2>
                 <div className='flex items-center gap-2'>
-                    <input type="text" placeholder='Input Text' className='border py-1 rounded-xl' />
-                    <button className='py-1 px-3 rounded-2xl bg-green-600 text-white'>Add</button>
+                    <input onChange={(e) => setS(e.target.value)} type="text" placeholder='Input Text' className='border py-1 rounded-xl' />
+                    <button onClick={a} className='py-1 px-3 rounded-2xl bg-green-600 text-white'>Add</button>
                 </div>
             </div>
         </div>
